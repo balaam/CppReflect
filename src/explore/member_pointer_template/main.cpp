b5c79ef969c7d07c4f560c045a3e91fcc4740182
Daniel Schuller <balaan@gmail.com>
2012-07-24 22:21:56 +0100

    Creating a class that act as 'metaclass' for reflection

--- src/explore/member_pointer_template/main.cpp
+++ src/explore/member_pointer_template/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <vector>
 
 //
 // Run with g++ main.cpp -o test
@@ -11,6 +12,47 @@ public:
     const char* string;
 };
 
+
+template<class T>
+class Field
+{
+private:
+    const char* mName;
+    void* T::* mPointer;
+public:
+    const char* Name() { return mName; }
+    Field(const char* name, void* T::* pointer) :
+        mName(name),
+        mPointer(pointer)
+    {}
+};
+
+// You couldn't have an array of these
+// But if they implemented an interface
+// you could
+template<class T>
+class Reflect
+{
+public:
+    std::vector< Field<T> > mFields;
+
+    void Print(T type)
+    {
+        typename std::vector< Field<T> >::iterator iter;
+        for (iter = mFields.begin();
+             iter != mFields.end(); ++iter)
+        {
+            printf("[%s]\n", iter->Name());
+
+            // Having cast away the type
+            // how do I get it back to print it! :)
+        }
+    }
+};
+
+// Iterate through the class and print it's members and values
+
+
 int main()
 {
     Test testInstance;
@@ -20,4 +62,13 @@ int main()
     printf("Instantiated struct. \n\tNum: %d. \n\tString: %s\n",
            testInstance.num,
            testInstance.string);
+
+    Reflect<Test> reflect;
+
+    reflect.mFields.push_back(Field<Test>(
+        "Num", (void* Test::*) &Test::num));
+    reflect.mFields.push_back(Field<Test>(
+        "String", (void* Test::*) &Test::string));
+
+    reflect.Print(testInstance);
 }
