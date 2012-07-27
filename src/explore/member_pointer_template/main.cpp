4da226299c4a7e84bc36a7c0acfeb324f8b36939
Daniel Schuller <balaan@gmail.com>
2012-07-27 20:15:14 +0100

    Working member reflection.

--- src/explore/member_pointer_template/main.cpp
+++ src/explore/member_pointer_template/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <vector>
-
+#include <string>
+#include <sstream>
 //
 // Run with g++ main.cpp -o test
 //
@@ -12,63 +13,114 @@ public:
     const char* string;
 };
 
+// 1. First go, but having thrown the type away
+//    I was stuck
+// template<class T>
+// class Field
+// {
+// private:
+//     const char* mName;
+//     void* T::* mPointer;
+// public:
+//     const char* Name() { return mName; }
+//     Field(const char* name, void* T::* pointer) :
+//         mName(name),
+//         mPointer(pointer)
+//     {}
+// };
 
-template<class T>
+// Abstract class, so children can have various types
+// We can have a list of abstract classes
 class Field
 {
+public:
+    virtual ~Field() {}
+    const char* Name() const { return mName; }
+    // This could be reflect ... probably!
+    virtual std::string ToString(void* instance) const = 0;
+protected:
+    Field(const char* name)  :
+        mName(name) {}
 private:
     const char* mName;
-    void* T::* mPointer;
-public:
-    const char* Name() { return mName; }
-    Field(const char* name, void* T::* pointer) :
-        mName(name),
-        mPointer(pointer)
-    {}
 };
+
+
+template<typename CLASS, typename TYPE>
+class FieldImpl : public Field
+{
+private:
+    TYPE CLASS::* mPointer;
+public:
+    FieldImpl(const char* name,  TYPE CLASS::* pointer):
+        Field(name),
+        mPointer(pointer)
+        {}
+
+    virtual std::string ToString(void* instance) const
+    {
+        // Throwing away the type can be avoided
+        // but this works as proof of concept.
+        CLASS* typedInstance = (CLASS*) instance;
+        std::stringstream ss;
+        ss << "[" << Name() << "]: "
+        << (*typedInstance).*mPointer;
+        return ss.str();
+    }
+
+};
+
 
 // You couldn't have an array of these
 // But if they implemented an interface
 // you could
-template<class T>
+template<typename T>
 class Reflect
 {
 public:
-    std::vector< Field<T> > mFields;
+    std::vector< Field* > mFields;
 
-    void Print(T type)
+    void Print(T* instance)
     {
-        typename std::vector< Field<T> >::iterator iter;
+        typename std::vector< Field* >::iterator iter;
         for (iter = mFields.begin();
              iter != mFields.end(); ++iter)
         {
-            printf("[%s]\n", iter->Name());
-
-            // Having cast away the type
-            // how do I get it back to print it! :)
+            printf("%s\n", (*iter)->ToString((void*)instance).c_str());
         }
+    }
+
+    template <typename CLASS, typename TYPE>
+    void AddField(const char* name, TYPE CLASS::* fieldPtr)
+    {
+        mFields.push_back(new FieldImpl<CLASS, TYPE>(name, fieldPtr));
     }
 };
 
-// Iterate through the class and print it's members and values
 
 
 int main()
 {
-    Test testInstance;
-    testInstance.num = 1;
-    testInstance.string = "Hello World.";
-
-    printf("Instantiated struct. \n\tNum: %d. \n\tString: %s\n",
-           testInstance.num,
-           testInstance.string);
-
     Reflect<Test> reflect;
+    reflect.AddField("num", &Test::num);
+    reflect.AddField("string", &Test::string);
 
-    reflect.mFields.push_back(Field<Test>(
-        "Num", (void* Test::*) &Test::num));
-    reflect.mFields.push_back(Field<Test>(
-        "String", (void* Test::*) &Test::string));
+    {
+        Test testInstance;
+        testInstance.num = 1;
+        testInstance.string = "Hello World.";
 
-    reflect.Print(testInstance);
+        printf("Instantiated struct. \n\tNum: %d. \n\tString: %s\n",
+               testInstance.num,
+               testInstance.string);
+
+        reflect.Print(&testInstance);
+    }
+
+    {
+        Test testInstance;
+        testInstance.num = 2;
+        testInstance.string = "Second test.";
+        reflect.Print(&testInstance);
+    }
 }
